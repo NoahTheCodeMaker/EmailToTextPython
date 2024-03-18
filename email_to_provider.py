e2584e07a6c5d164552ccc82eabad23b5cda7413
NoahDragoon <ndragoon@acelynk.com>
2024-03-18 17:15:50 -0400

    Add main script to send email to cell provider

---
+++ email_to_provider.py
@@ -0,0 +1,37 @@
+import smtplib, ssl, os
+from cell_providers import PROVIDERS
+from receiver_list import RECEIVERS
+from message_type import MESSAGES
+
+def send_sms_via_email(
+    number: str,
+    message: str,
+    provider: str,
+    sender_credentials: tuple,
+    subject: str = "Sent using AWS and python",
+    smtp_server: str = "smtp.gmail.com",
+    smtp_port: int = 465,
+):
+    
+    sender_email, email_password = sender_credentials
+    receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
+    email_message = f"Subject:{subject}\nTo:{receiver_email}\n{message}"
+
+    with smtplib.SMTP_SSL(
+        smtp_server, smtp_port, context=ssl.create_default_context()
+    ) as email:
+        email.login(sender_email, email_password)
+        email.sendmail(sender_email, receiver_email, email_message)
+
+def main():
+    sender_credentials = (os.getenv('SMTP_EMAIL'), os.getenv('SMTP_PASSWORD'))
+    for name, details in RECEIVERS.items():
+        number = details['phone_number']
+        message = f'Greetings {name}! \n'
+        for message_type in details['message_type']:
+            message += MESSAGES[message_type] + '\n'
+        provider = details['provider']
+        send_sms_via_email(number, message, provider, sender_credentials)
+
+if __name__ == "__main__":
+    main()
